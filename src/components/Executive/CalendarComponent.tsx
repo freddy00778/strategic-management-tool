f93925ab864778ca3d6a395a3deb01d3be30b3da
Nanakwame <44173438+nanakwame667@users.noreply.github.com>
2023-08-03 10:50:20 +0000

    added the shapes to the calendar

--- src/components/Executive/CalendarComponent.tsx
+++ src/components/Executive/CalendarComponent.tsx
@@ -22,6 +22,7 @@ export type Event = {
   border?: string;
   width?: number;
   extendedProps?: object;
+  type?: string;
 };
 
 const CalendarComponent: React.FC<DemoAppState> = ({
@@ -52,16 +53,47 @@ const CalendarComponent: React.FC<DemoAppState> = ({
   };
 
   const renderEventContent = (eventContent: EventContentArg) => {
-    return (
-      <div className="bg-transparent flex flex-wrap gap-2 w-12">
+    const eventType = eventContent.event.extendedProps.type;
+    const eventColor = eventContent.event.textColor;
+
+    if (eventType === "milestone") {
+      return (
         <div
-          className="w-12 h-6  rounded-md flex items-center justify-center"
-          title={eventContent.event.title}
-        >
-          <i className="text-[10px]">{eventContent.event.id}</i>
+          style={{ backgroundColor: eventColor }}
+          className="w-3 h-3 transform rotate-45"
+        ></div>
+      );
+    } else if (eventType === "activity") {
+      return (
+        <div
+          className="w-4 h-4 rounded-full"
+          style={{ backgroundColor: eventColor }}
+        ></div>
+      );
+    } else if (eventType === "initiative") {
+      return (
+        <div
+          style={{
+            width: 0,
+            height: 0,
+            borderLeft: "8px solid transparent",
+            borderRight: "8px solid transparent",
+            borderBottom: `16px solid ${eventColor}`,
+          }}
+        ></div>
+      );
+    } else {
+      return (
+        <div className="bg-transparent flex flex-wrap gap-2 w-12">
+          <div
+            className="w-12 h-6 rounded-full flex items-center justify-center"
+            title={eventContent.event.title}
+          >
+            <i className="text-[10px]">{eventContent.event.id}</i>
+          </div>
         </div>
-      </div>
-    );
+      );
+    }
   };
 
   return (
